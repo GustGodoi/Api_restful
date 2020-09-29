67c877c8a87e7b8e259b0463cb6aa600051bec1e
Gustavo Godoi <gustavogodoi442@gmail.com>
2020-09-28 21:38:36 -0300

    28-09-2020
    erros

--- js/UsuarioView.js
+++ js/UsuarioView.js
@@ -1,89 +1,99 @@
 function exibirUsuarios(usuarios) {
-
-    popularTabela(usuarios);
+    linhas = document.getElementById('listaUsuarios').rows;
+    limparTabela(linhas)
+    popularTabela(usuarios)
 }
 
-function popularTabela(usuarios) {
-    for (var i = 0; i < usuarios.length; i++) {
-        inserirLinhaTabela(usuarios[i]);
+
+function limparTabela(linhas){
+    for (i= linhas.length-1; i>=0; i--){
+        excluirLinhaTabela(i)
+    }
+    
+}
+
+function excluirLinhaTabela(i){
+    document.getElementById('listaUsuarios').deleteRow(i);
+}
+
+function popularTabela(usuarios){
+    for(i = 0; i < usuarios.length; i++){
+        exibirLinhaTabela(usuarios[i]);
     }
 }
 
-function inserirLinhaTabela(usuario) {
-    var tabela = document.getElementById('tabelaDeUsuarios');
-    var numLinhas = tabela.rows.length;
-    var novaLinha = tabela.insertRow(numLinhas);
+function exibirLinhaTabela(usuario){
 
-    var celCodigo = novaLinha.insertCell(0);
+    let table = document.getElementById('listaUsuarios');
+    let numLinhas = table.rows.length;
+    let novaLinha = table.insertRow(numLinhas);
+
+    let celCodigo = novaLinha.insertCell(0);
     celCodigo.innerHTML = usuario.codigo;
 
-    var celNome = novaLinha.insertCell(1);
+    let celNome = novaLinha.insertCell(1);
     celNome.innerHTML = usuario.nome;
 
-    var celEmail = novaLinha.insertCell(2);
+    let celEmail = novaLinha.insertCell(2);
     celEmail.innerHTML = usuario.email;
 
-    var celLogin = novaLinha.insertCell(3);
+    let celLogin = novaLinha.insertCell(3);
     celLogin.innerHTML = usuario.login;
 
+    
 }
 
 function validarUsuario(acao) {
-    var nome = document.getElementById('nome');
-    var email = document.getElementById('email');
-    var login = document.getElementById('login');
-    var senha = document.getElementById('senha');
-    var senhaValidar = document.getElementById('senhaValidar');
 
-    if (acao == "add") {
-        var dadosValidos = true;
+    let nome = document.getElementById('nome');
+    let email = document.getElementById('email');
+    let login = document.getElementById('login');
+    let senha = document.getElementById('senha');
+    let validarSenha = document.getElementById('validarSenha');
 
-        if (nome.value == "") {
+    if (acao === 'add') {
+        let dadosValidos = true;
+
+        if (nome.value == '') {
             dadosValidos = false;
-            alert("Preencha o campo nome");
-        } else if (email.value == "") {
+            alert('Preencha o campo nome');
+        } else if (email.value == '') {
             dadosValidos = false;
-            alert("Informe um e-mail");
-        } else if (login.value == "") {
+            alert('Preencha o campo email');            
+        } else if (login.value == '') {
             dadosValidos = false;
-            alert("Preencha o campo login");
+            alert('Preencha o campo login');            
         } else {
-            if (senha.value != "") {
-                if (senha.value == senhaValidar.value) {
-                    var senhaTxt = senha.value;
-
-                    if (senhaTxt.length < 6) {
+            if(senha.value != '') {
+                if (validarSenha.value == senha.value) {
+                    if(senha.value.length > 6){
                         dadosValidos = false;
-                        alert("A senha precisa ter pelo menos 6 caracteres");
+                        alert('A precisa ao menos 6 carcteres');                
                     }
                 } else {
                     dadosValidos = false;
-                    alert("As senhas informadas não são iguais");
+                    alert('As senhas devem coincidir');            
                 }
-
             } else {
                 dadosValidos = false;
-                alert("Informe a senha");
+                alert('Preencha o campo senha');            
             }
         }
-
         if (dadosValidos) {
-            //Enviar para a API
-            var objUsuario = {
+            let objUsuario = {
                 "nome": nome.value,
                 "email": email.value,
                 "login": login.value,
-                "senha": senha.value
-            };
-            //Adiciona o novo usuário
-            adicionarUsuario(objUsuario);
-        }
+                "senha": senha.value,
+            }
 
+            adicionarUsuarios(objUsuario);
+        }
     }
 
     return false;
 }
 
-window.onload = function () {
-    getUsuarios();
-};
+window.onload = function(){
+    this.getUsuarios();
+}
